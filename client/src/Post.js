cbdf2161c91b362eb68be27817bed42642c1b713
vishal989 <vavishalarya@gmail.com>
2023-05-20 16:48:26 +0530

    some frontend

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,24 +1,29 @@
-import {formatISO9075} from "date-fns";
-import {Link} from "react-router-dom";
+// import {formatISO9075} from "date-fns";
+import { Link } from 'react-router-dom';
 
-export default function Post({_id,title,summary,cover,content,createdAt,author}) {
-
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
   return (
     <div className="post">
       <div className="image">
-        <Link to={`/post/${_id}`}>
-          <img src={'http://localhost:4000/'+cover} alt=""/>
-        </Link>
+        <img src="https://logos-world.net/wp-content/uploads/2021/11/GoKart-New-Logo.png" alt="" />
       </div>
       <div className="texts">
-        <Link to={`/post/${_id}`}>
-        <h2>{title}</h2>
-        </Link>
+        <h2>GO Cart</h2>
         <p className="info">
-          <a className="author">{author.username}</a>
-          <time>{formatISO9075(new Date(createdAt))}</time>
+          <a className="author">Arya</a>
+          <time>
+            2023-10-19 03:19
+          </time>
         </p>
-        <p className="summary">{summary}</p>
+        <p className="summary">This is the summary for the Go-Cart. It's super fast. So is your mom. Bring your mom, we are gonna get koochies for tonight.</p>
       </div>
     </div>
   );
